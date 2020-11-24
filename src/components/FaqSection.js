0026e0e2b2ee47886c36c1be2b399d752ce71a64
Dong Nguyen <39297115+s3634096@users.noreply.github.com>
2020-11-24 14:48:53 +0700

    Add Q&A Section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+
+const FaqSection = () => {
+    return (
+        <div className="faq">
+            <h2>Any Questions <span>FAQ</span></h2>
+            <div className="question">
+                <h4>How Do I Start?</h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error tempora unde et fugit esse ea recusandae aperiam. Dolorem natus cumque facilis, exercitationem necessitatibus itaque tenetur, ipsa, minus cum blanditiis iste?</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error tempora unde et fugit esse ea recusandae aperiam. Dolorem natus cumque facilis, exercitationem necessitatibus itaque tenetur, ipsa, minus cum blanditiis iste?</p>
+            </div>
+            <div className="question">
+                <h4>How Do I Start?</h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error tempora unde et fugit esse ea recusandae aperiam. Dolorem natus cumque facilis, exercitationem necessitatibus itaque tenetur, ipsa, minus cum blanditiis iste?</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error tempora unde et fugit esse ea recusandae aperiam. Dolorem natus cumque facilis, exercitationem necessitatibus itaque tenetur, ipsa, minus cum blanditiis iste?</p>
+            </div>
+            <div className="question">
+                <h4>Daily Schedule</h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error tempora unde et fugit esse ea recusandae aperiam. Dolorem natus cumque facilis, exercitationem necessitatibus itaque tenetur, ipsa, minus cum blanditiis iste?</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error tempora unde et fugit esse ea recusandae aperiam. Dolorem natus cumque facilis, exercitationem necessitatibus itaque tenetur, ipsa, minus cum blanditiis iste?</p>
+            </div>
+            <div className="question">
+                <h4>Different Payment Methods</h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error tempora unde et fugit esse ea recusandae aperiam. Dolorem natus cumque facilis, exercitationem necessitatibus itaque tenetur, ipsa, minus cum blanditiis iste?</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error tempora unde et fugit esse ea recusandae aperiam. Dolorem natus cumque facilis, exercitationem necessitatibus itaque tenetur, ipsa, minus cum blanditiis iste?</p>
+            </div>
+            <div className="question">
+                <h4>What Products do you offer?</h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error tempora unde et fugit esse ea recusandae aperiam. Dolorem natus cumque facilis, exercitationem necessitatibus itaque tenetur, ipsa, minus cum blanditiis iste?</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error tempora unde et fugit esse ea recusandae aperiam. Dolorem natus cumque facilis, exercitationem necessitatibus itaque tenetur, ipsa, minus cum blanditiis iste?</p>
+            </div>
+        </div>
+
+    )
+}
+
+export default FaqSection;
