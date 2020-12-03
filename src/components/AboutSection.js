21f4e98b333f8419f8987f48725e3b29aa1f9b9d
Dong Nguyen <39297115+s3634096@users.noreply.github.com>
2020-12-03 17:04:26 +0700

    Update animation in About page

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -4,32 +4,41 @@ import home1 from "../img/home1.png"
 import { About, Description, Image, Hidden} from "../styles"
 // Framer Motion
 import {motion} from "framer-motion";
-
+import {titleAnimation, fade, photoAnimation} from "../animation";
 const AboutSection = () => {
-
     return (
         <About> 
             <Description>
                 <motion.div className="title">
                     <Hidden>
                         <motion.h2
+                            variants={titleAnimation}
                         >We work to make</motion.h2>
                     </Hidden>
                     <Hidden>
                         <motion.h2
+                            variants={titleAnimation}
                         >your <span>dreams</span></motion.h2>
                     </Hidden>
                     <Hidden>
                         <motion.h2
+                            variants={titleAnimation}
                         >come true.</motion.h2>
                     </Hidden>
                 </motion.div>
-                <p>Contact us for any photography or videography ideas 
-                that you have. We have professionals with amazing skills.</p>
-                <button>Contact us</button>  
+                <motion.p
+                    variants={fade}
+                >Contact us for any photography or videography ideas 
+                    that you have. We have professionals with amazing skills.
+                </motion.p>
+                <motion.button variants={fade}>Contact us</motion.button>  
             </Description>
             <Image>
-                <img src={home1} alt="Guy with a camera"/>
+                <motion.img
+                    src={home1}
+                    alt="Guy with a camera"
+                    variants={photoAnimation}
+                />
             </Image>
         </About>
     )
